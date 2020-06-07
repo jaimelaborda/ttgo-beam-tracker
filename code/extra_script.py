77270a19deb4dae0aac357e8fe19aff0b28edb4f
Jaime Laborda <jlaborda@itaca.upv.es>
2020-06-07 19:27:33 +0200

    Changed PROJECT environment variable and display library name

--- code/extra_script.py
+++ code/extra_script.py
@@ -4,7 +4,7 @@ Import("env", "projenv")
 
 # Get current environment
 environment = env['PIOENV']
-libdeps = env['PROJECTLIBDEPS_DIR']
+libdeps = env['PROJECT_LIBDEPS_DIR']
 file = "lmic_project_config.h"
 source = "src/{}".format(file)
 destination = "{}/{}/MCCI LoRaWAN LMIC library/project_config/{}".format(libdeps, environment, file)
